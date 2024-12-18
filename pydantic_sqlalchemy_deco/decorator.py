3e1d14771af8d1249949b4f204c738ce4c45f70f
Kevin Evans <evans.kevinh@gmail.com>
2024-12-18 09:31:40 -0800

    feat: ability to adjust json/jsonb

--- pydantic_sqlalchemy_deco/decorator.py
+++ pydantic_sqlalchemy_deco/decorator.py
@@ -1,7 +1,7 @@
 import sys
 from typing import Any, Type, Optional, TypeVar
 from pydantic import BaseModel
-from sqlalchemy.types import TypeDecorator
+from sqlalchemy.types import TypeDecorator, JSON
 from sqlalchemy.dialects.postgresql import JSONB
 
 if sys.version_info >= (3, 13):
@@ -12,14 +12,17 @@ else:
 class PydanticJSON(TypeDecorator[T]):
     impl = JSONB
 
-    def __init__(self, pydantic_model: Type[T]) -> None:
+    def __init__(self, pydantic_model: Type[T], impl: Type[JSON] = JSONB) -> None:
         """
         Initializes the custom type with the Pydantic model to use for serialization/deserialization.
 
         :param pydantic_model: The Pydantic model class to use for JSON handling.
         """
-        super().__init__()
+        
+        self.__class__.impl = impl
         self.pydantic_model = pydantic_model
+
+        super().__init__()
 
     def process_bind_param(self, value: Optional[T], dialect: Any) -> Optional[Any]:
         if value is None:
